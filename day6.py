6ad04f41011e325a9af7b203e7a887b13e148839
nuralisa16 <99975163+nuralisa16@users.noreply.github.com>
2023-10-14 14:03:03 +0800

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,10 @@
+#input
+nama = input("masukkan nama anda : ")
+umur = int(input("masukkan umur : "))
+alamat = input("masukkan alamat : ")
+Hoby = input("masukkan hoby : ")
+
+print("nama   : ",nama)
+print("umur   : ", umur)
+print("alamat : ",alamat)
+print("Hoby   : ",Hoby)
