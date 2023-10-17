01900de6b9b7896ce20d20ef0da6b1bb757b866e
nuralisa16 <99975163+nuralisa16@users.noreply.github.com>
2023-10-17 19:27:14 +0800

    Add files via upload

---
+++ day9.py
@@ -0,0 +1,7 @@
+username = input("Username anda : ")
+password = int(input("pasword anda : "))
+
+if username == "lisa" and password == 123 :
+    print("Ya Kamu adalah Lisa Comel")
+else:
+    print("Kamu bukan lisa comel")
