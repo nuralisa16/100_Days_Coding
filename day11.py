5d174bff737ad8648a1d8eb5650bdf008320d8c4
nuralisa16 <99975163+nuralisa16@users.noreply.github.com>
2023-10-20 05:50:26 +0800

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,9 @@
+PCGaming = 150000000
+Penghasilan = 13000000
+Pengeluaran = 7000000
+TabunganPerBulan = 5000000
+SatuTahun = 12
+Target = TabunganPerBulan * SatuTahun
+Jumlah = PCGaming / Target
+
+print("Sambo Harus menabung selama :",Jumlah , "Tahun")
