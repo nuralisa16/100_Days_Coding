37d4290bc18c30bb031476c493276d311294d526
nuralisa16 <99975163+nuralisa16@users.noreply.github.com>
2023-11-29 20:01:06 +0800

    Perbandingan yang menginput nilai

---
+++ day52.py
@@ -0,0 +1,9 @@
+nilai = int(input("masukan nilai : "));
+if nilai >= 80  and nilai <=100:
+    print ("nilai anda adalah A")
+elif nilai >=70 and nilai <=80:
+    print("nilai anda adalah B")
+elif nilai >= 60 and nilai <=70 :
+    print("nilai anda adalah C")
+else :
+    print("error")
