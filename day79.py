42fafc4c4639d3b2121a723137a0b5f58277dd3b
nuralisa16 <99975163+nuralisa16@users.noreply.github.com>
2023-12-26 19:20:25 +0800

    Mencari nilai maksimum menggunakan fungsi def

---
+++ day79.py
@@ -0,0 +1,6 @@
+def nilai_maksimum(angka1, angka2, angka3):
+    maksimum = max(angka1, angka2, angka3)
+    return f"Nilai maksimum adalah {maksimum}"
+
+hasil = nilai_maksimum(15, 8, 22)
+print(hasil)
