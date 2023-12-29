9296fdb9b6aaa1a48df29c7327de0a6e916c1c0c
nuralisa16 <99975163+nuralisa16@users.noreply.github.com>
2023-12-29 20:46:22 +0800

    Operasi pengurangan menggunakan fungsi input dan fungsi def

---
+++ day82.py
@@ -0,0 +1,10 @@
+def jumlahkan_bilangan(a, b):
+    hasil = a - b
+    return hasil
+
+bilangan1 = float(input("Masukkan bilangan pertama: "))
+bilangan2 = float(input("Masukkan bilangan kedua: "))
+
+total = jumlahkan_bilangan(bilangan1, bilangan2)
+
+print("Hasil penjumlahan:", total)
