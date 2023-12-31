99ba9d659b075be87ebb46eac7ee2538e85f506b
nuralisa16 <99975163+nuralisa16@users.noreply.github.com>
2023-12-31 08:15:43 +0800

    Mencari hasil pangkat menggunakan input dan fungsi def

---
+++ day84.py
@@ -0,0 +1,10 @@
+def hasil_bilangan(a, b):
+    hasil = a ** b
+    return hasil
+
+bilangan = float(input("Masukkan bilangan pertama: "))
+pangkat = float(input("Masukkan pangkat: "))
+
+total = hasil_bilangan(bilangan1, bilangan2)
+
+print("Hasil pangkat :", total)
