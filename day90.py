f4069b0a8d0a389021e398f9b282adfbf1d38d86
nuralisa16 <99975163+nuralisa16@users.noreply.github.com>
2024-01-07 20:27:38 +0800

    Array campuran menggunakan fungsi def

---
+++ day90.py
@@ -0,0 +1,9 @@
+def array_campuran(angka, string):
+    campuran = [angka, string]
+    return campuran
+angka = 16
+teks = 'Lisa'
+hasil = array_campuran(angka, teks)
+print("Array Campuran:", hasil)
+
+
